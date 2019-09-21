11ddc18d82ddb23483eba3a1f3d8bcbfdab677cd
jacobrobledo <46408627+Robledo-Jacob@users.noreply.github.com>
2019-09-21 13:53:23 -0600

    3

--- lastmod.js
+++ lastmod.js
@@ -1,7 +1,14 @@
-function lastModified(){
-    document.write(document.lastModified);
-       
-   }
-   
-   last.Modified();
-   
+var options = {
+    weekday: 'long', 
+    day: 'numeric', 
+    month: 'long', 
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+}
+
+document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-US', options);
+
+document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
+
