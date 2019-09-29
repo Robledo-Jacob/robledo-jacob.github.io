1b3456c99f6623c5281e94f7bc03149c7f0a6752
jacobrobledo <46408627+Robledo-Jacob@users.noreply.github.com>
2019-09-28 20:26:14 -0600

    gogo

--- lesson 2/lastmod.js
+++ lesson 2/lastmod.js
@@ -1,7 +1,7 @@
 var options = {
-    weekday: 'long', 
-    day: 'numeric', 
-    month: 'long', 
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
     year: 'numeric',
     hour: 'numeric',
     minute: 'numeric',
@@ -9,6 +9,3 @@ var options = {
 }
 
 document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-US', options);
-
-
-
