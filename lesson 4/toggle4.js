8dd1656955e5607bf2cf045c6a42ee967e0b8e8a
jacobrobledo <46408627+Robledo-Jacob@users.noreply.github.com>
2019-10-12 16:54:03 -0600

    ddddd

--- lesson 4/toggle4.js
+++ lesson 4/toggle4.js
@@ -9,9 +9,7 @@ var options = {
     day: 'numeric', 
     month: 'long', 
     year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
+ 
 }
 
 document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-US', options);
